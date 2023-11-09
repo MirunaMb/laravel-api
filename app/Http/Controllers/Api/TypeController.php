8dd4f13365dfa75c3308a7eb5924d2f0111dd661
MirunaMb <mirunamarcela98@gmail.com>
2023-11-09 16:53:11 +0100

    create TypeController

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Type; //importo il modello di Type
+
+class TypeController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //manca il nome della type 
+        //come facciamo a sostiture il nome_type con il type ?
+        //mi trovo il type
+        $type = Type::select('id','name','color')
+         ->where('id', $id)
+        ->first();
+
+        if(!$type)
+        abort(404,'Category not found');
+
+        return response()->json($type);
+    }
+    
+}
