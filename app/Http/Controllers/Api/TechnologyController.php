71bd14e504fcdcfe1d6786ff07b4e6f1fdbce16a
MirunaMb <mirunamarcela98@gmail.com>
2023-11-11 00:26:41 +0100

    create TechnologyController

---
+++ app/Http/Controllers/Api/TechnologyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $technology = Technology::select('id','name_technologies')->get();
+       return response()->json($technology);
+    }
+}
